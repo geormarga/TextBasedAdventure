f12a5a8d31729856ded76c40e5f4d231d6fe07b8
George Margaritis <geormarga@gmail.com>
2017-04-12 22:21:32 +0300

    State now gets room and item values from Map.xml and Items.xml respectively

--- src/main/java/textbasedadventure/State.java
+++ src/main/java/textbasedadventure/State.java
@@ -7,37 +7,17 @@ package textbasedadventure;
 
 import features.FeatureFactory;
 import items.*;
-
-import items.containers.MoldyTree;
-import items.containers.CommonChest;
-import items.containers.GoldenChest;
-import items.containers.Hole;
-import items.containers.Mudpit;
-import items.containers.SilverChest;
-
-import rooms.AbandonedTemple;
-import rooms.Castle;
-import rooms.Cave;
-import rooms.DeadEnd;
-import rooms.Dungeon;
-import rooms.Forest;
-import rooms.Hall;
-import rooms.HiddenRoom;
-import rooms.Keep;
-import rooms.LivingRoom;
-import rooms.Room;
-import rooms.StartingRoom;
-import rooms.Study;
+import items.containers.*;
+import rooms.*;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.LinkedList;
 
 public class State implements Serializable {
 
     private Room currentRoom;
     private Room previousRoom;
     private Inventory inventory;
+    private ReadXMLFile readXMLFile = new ReadXMLFile();
 
     private final FeatureFactory featureFactory = new FeatureFactory();
 
@@ -69,56 +49,25 @@ public class State implements Serializable {
         return featureFactory;
     }
 
+    public ReadXMLFile getReadXMLFile() {
+        return this.readXMLFile;
+    }
+
     public State() {
 
         //Create Rooms
-        Room forest = new Forest(this.featureFactory);
-        forest.setNearbyRooms(new LinkedList<>(Arrays.asList("dead end", "castle", "keep")));
-        forest.setRoomItems(new LinkedList<>(Arrays.asList("rusty key", "moldy tree", "sundial")));
-
-        Room abandonedTemple = new AbandonedTemple(this.featureFactory);
-        abandonedTemple.setNearbyRooms(new LinkedList<>(Arrays.asList("keep")));
-        abandonedTemple.setRoomItems(new LinkedList<>(Arrays.asList("statue", "altar")));
-
-        Room castle = new Castle(this.featureFactory);
-        castle.setNearbyRooms(new LinkedList<>(Arrays.asList("forest", "hall", "cave")));
-        castle.setRoomItems(new LinkedList<>(Arrays.asList("common chest")));
-
-        Room cave = new Cave(this.featureFactory);
-        cave.setNearbyRooms(new LinkedList<>(Arrays.asList("castle")));
-        cave.setRoomItems(new LinkedList<>(Arrays.asList("old man", "rock")));
-
-        Room deadEnd = new DeadEnd(this.featureFactory);
-        deadEnd.setNearbyRooms(new LinkedList<>(Arrays.asList("forest", "dungeon")));
-        deadEnd.setRoomItems(new LinkedList<>(Arrays.asList("hole", "wooden wheel", "hatch")));
-
-        Room dungeon = new Dungeon(this.featureFactory);
-        dungeon.setNearbyRooms(new LinkedList<>(Arrays.asList("hall")));
-        dungeon.setRoomItems(new LinkedList<>(Arrays.asList("water", "corpse", "parchment")));
-
-        Room hall = new Hall(this.featureFactory);
-        hall.setNearbyRooms(new LinkedList<>(Arrays.asList("castle", "living room", "dungeon", "study")));
-        hall.setRoomItems(new LinkedList<>(Arrays.asList("common chest", "rusty key")));
-
-        Room hiddenRoom = new HiddenRoom(this.featureFactory);
-        hiddenRoom.setNearbyRooms(new LinkedList<>(Arrays.asList("study")));
-        hiddenRoom.setRoomItems(new LinkedList<>(Arrays.asList("stand", "triangular artifact")));
-
-        Room keep = new Keep(this.featureFactory);
-        keep.setNearbyRooms(new LinkedList<>(Arrays.asList("forest", "abandoned temple")));
-        keep.setRoomItems(new LinkedList<>(Arrays.asList("silver chest", "mudpit", "silver key")));
-
-        Room livingRoom = new LivingRoom(this.featureFactory);
-        livingRoom.setNearbyRooms(new LinkedList<>(Arrays.asList("hall")));
-        livingRoom.setRoomItems(new LinkedList<>(Arrays.asList()));
-
-        Room startingRoom = new StartingRoom(this.featureFactory);
-        startingRoom.setNearbyRooms(new LinkedList<>(Arrays.asList("forest")));
-        startingRoom.setRoomItems(new LinkedList<>(Arrays.asList()));
-
-        Room study = new Study(this.featureFactory);
-        study.setNearbyRooms(new LinkedList<>(Arrays.asList("hall", "dungeon", "hidden room")));
-        study.setRoomItems(new LinkedList<>(Arrays.asList("book", "bookshelf")));
+        new Forest(this.featureFactory, this.readXMLFile);
+        new AbandonedTemple(this.featureFactory, this.readXMLFile);
+        new Castle(this.featureFactory, this.readXMLFile);
+        new Cave(this.featureFactory, this.readXMLFile);
+        new DeadEnd(this.featureFactory, this.readXMLFile);
+        new Dungeon(this.featureFactory, this.readXMLFile);
+        new Hall(this.featureFactory, this.readXMLFile);
+        new HiddenRoom(this.featureFactory, this.readXMLFile);
+        new Keep(this.featureFactory, this.readXMLFile);
+        new LivingRoom(this.featureFactory, this.readXMLFile);
+        new Study(this.featureFactory, this.readXMLFile);
+        Room startingRoom = new StartingRoom(this.featureFactory, this.readXMLFile);
 
         //Create Items
         new RustyKey(this.featureFactory);
