fd2b4cb17da9aed3702b72fe96b6078bf0992ae3
George Margaritis <georgios.margaritis@klarna.com>
2020-12-24 14:12:58 +0100

    Remove unused method

--- src/main/java/textbasedadventure/webapp/game/parsing/JSONParser.java
+++ src/main/java/textbasedadventure/webapp/game/parsing/JSONParser.java
@@ -2,33 +2,11 @@ package textbasedadventure.webapp.game.parsing;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class JSONParser {
-
-
-    /**
-     * Method that takes a NodeList and keeps only the nodes of type ELEMENT
-     *
-     * @param nodeList the NodeList given
-     * @return Cleared out element list
-     */
-    public static List<Element> toElementList(NodeList nodeList) {
-        List<Element> elementList = new ArrayList<>();
-        for (int index = 0; index < nodeList.getLength(); index++) {
-            if (nodeList.item(index).getNodeType() == Node.ELEMENT_NODE) {
-                elementList.add((Element) nodeList.item(index));
-            }
-        }
-        return elementList;
-    }
 
     /**
      * Method that parses the a json File to a json array
