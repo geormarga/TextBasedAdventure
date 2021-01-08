8a7aa536180df15495d39e152409bc81f08a2049
George Margaritis <georgios.margaritis@klarna.com>
2021-01-08 17:20:56 +0100

    Study: Fix typo

--- src/main/java/textbasedadventure/webapp/game/rooms/LivingRoom.java
+++ src/main/java/textbasedadventure/webapp/game/rooms/LivingRoom.java
@@ -20,7 +20,7 @@ public class LivingRoom extends Room {
     @Autowired
     public LivingRoom(Map map) {
         name = "living room";
-        description = "This is the main Dining Room. The plates on the table indicated that this castle has long been abandonded.";
+        description = "This is the main Dining Room. The plates on the table indicated that this castle has long been abandoned.";
         hint = "This room is a mess... something happened here!";
         roomItems= map.getRoomItems(name);
     }
