0d9779d77c9af3d5be3325603dc301bfd336a983
George Margaritis <georgios.margaritis@klarna.com>
2021-01-09 13:26:57 +0100

    State: Rearrange imports

--- src/main/java/textbasedadventure/webapp/game/State.java
+++ src/main/java/textbasedadventure/webapp/game/State.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package textbasedadventure.webapp.game;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import textbasedadventure.webapp.game.features.FeatureFactory;
-import textbasedadventure.webapp.game.rooms.Room;
 
-import javax.annotation.PostConstruct;
+import textbasedadventure.webapp.game.features.FeatureFactory;
+import textbasedadventure.webapp.game.items.containers.Inventory;
+import textbasedadventure.webapp.game.rooms.Room;
 
 @Component
 public class State {
